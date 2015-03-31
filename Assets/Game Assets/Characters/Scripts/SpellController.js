efa96b954c4bbbc591865e36c3e40ce6a7cda541
oneillkyle <oneillkyle@gmail.com>
2015-03-30 21:25:57 -0500

    Added scene
    Added first scene with simple scripts

---
+++ Assets/Game Assets/Characters/Scripts/SpellController.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+class SpellBook{
+	var spellOne: GameObject;
+	var spellTwo: GameObject;
+	var spellThree: GameObject;
+	var spellFour: GameObject;
+}
+
+var spellBook = SpellBook();
+
+function Start () {
+}
+
+function Update () {
+	
+}
